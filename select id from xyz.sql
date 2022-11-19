09d3b8be49cc117a8f11d57f02f410d45af81f76
Gourav <dharg57@gmail.com>
2022-11-19 12:18:50 +0530

    Create select id from xyz.sql

---
+++ select id from xyz.sql
@@ -0,0 +1,6 @@
+SET @start_time = ROUND(UNIX_TIMESTAMP(CURTIME(4)) * 1000);
+ 
+select film_id FROM sakila.film where film_id = 250;
+SET @end_time = ROUND(UNIX_TIMESTAMP(CURTIME(4)) * 1000);
+ 
+SELECT (@end_time - @start_time) AS difference;
