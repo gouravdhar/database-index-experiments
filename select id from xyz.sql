13b9d75077cbc117501273cfbbdbd7ebbd8d7f69
Gourav <dharg57@gmail.com>
2022-11-19 12:21:35 +0530

    Update select id from xyz.sql

--- select id from xyz.sql
+++ select id from xyz.sql
@@ -3,4 +3,4 @@ SET @start_time = ROUND(UNIX_TIMESTAMP(CURTIME(4)) * 1000);
 select film_id FROM sakila.film where film_id = 250;
 SET @end_time = ROUND(UNIX_TIMESTAMP(CURTIME(4)) * 1000);
  
-SELECT (@end_time - @start_time) AS difference;
+SELECT (@end_time - @start_time) AS time_taken;
